6251f5fe319311bf97bade36f7d60eb7a8fe6326
kgryte <kgryte@gmail.com>
2015-04-28 23:27:21 -0800

    [UPDATE] Refactor. Split into sep fcns. Query class. Emit evts. Extract ratelimit info.

--- TODO.md
+++ TODO.md
@@ -1,2 +1,20 @@
 TODO
 ====
+
+1. 	Class or an event-emitter?
+	-	how to provide poll progress?
+		-	number of open requests?
+		-	failed requests?
+		-	could be used to allow graceful exit
+			-	e.g., wait for all open requests to return before exit
+	- 	return a `Query` class, which is also an event-emitter
+		- events
+			-	request
+			-	page
+			-	data
+			-	error
+			-	start
+			-	stop
+			-	end
+			-	init
+2. 	
--- examples/index.js
+++ examples/index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var get = require( './../lib' );
+var createQuery = require( './../lib' );
 
 var opts = {
 	'uri': 'https://api.github.com/user/repos',
@@ -10,20 +10,42 @@ var opts = {
 
 		// INSERT TOKEN HERE //
 		'Authorization': 'token <your_token_goes_here>'
+
+		//
 	},
 	'qs': {
 		'page': 1,
 		'per_page': 100
 	},
-	'all': true
+	'all': true,
+	'interval': 10000
 };
 
-get( opts, onResponse );
+var query = createQuery( opts );
 
-function onResponse( error, body ) {
-	if ( error ) {
-		console.error( error );
-		return;
-	}
-	console.log( body );
+query.on( 'error', onError );
+query.on( 'request', onRequest );
+query.on( 'page', onPage );
+query.on( 'data', onData );
+query.on( 'end', onEnd );
+
+setTimeout( function stop() {
+	query.stop();
+}, 60000 );
+
+function onError( evt ) {
+	console.error( evt );
+}
+function onRequest( evt ) {
+	console.log( 'Query %d request...', evt.qid );
+}
+function onPage( evt ) {
+	var progress = evt.count / evt.total * 100;
+	console.log( 'Query %d progress: %d%.' , evt.qid, Math.round( progress ) );
+}
+function onData( evt ) {
+	console.log( evt.ratelimit );
+}
+function onEnd( id ) {
+	console.log( 'Query %d ended...', id );
 }
