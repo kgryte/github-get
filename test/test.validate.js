8cc732e98562df6b30a9b1d209cc75ead04a1745
kgryte <kgryte@gmail.com>
2016-02-06 01:58:50 -0800

    Add option validation tests

--- test/test.validate.js
+++ test/test.validate.js
@@ -36,13 +36,303 @@ tape( 'if an options argument is not an object, the function returns a type erro
 	t.end();
 });
 
+tape( 'if provided a `protocol` option which is not a primitive string, the function returns a type error', function test( t ) {
+	var values;
+	var err;
+	var i;
+
+	values = [
+		5,
+		NaN,
+		null,
+		undefined,
+		true,
+		[],
+		{},
+		function(){}
+	];
+
+	for ( i = 0; i < values.length; i++ ) {
+		err = validate( {}, {
+			'protocol': values[i]
+		});
+		t.ok( err instanceof TypeError, 'returns type error when provided ' + values[i] );
+	}
+	t.end();
+});
+
+tape( 'if provided a `protocol` option which is not either `https` or `http`, the function returns an error', function test( t ) {
+	var values;
+	var err;
+	var i;
+
+	values = [
+		'beep',
+		'boop',
+		'bap',
+		'ftp',
+		'smtp',
+		'udp'
+	];
+
+	for ( i = 0; i < values.length; i++ ) {
+		err = validate( {}, {
+			'protocol': values[i]
+		});
+		t.ok( err instanceof Error, 'returns an error when provided ' + values[i] );
+	}
+	t.end();
+});
+
+tape( 'if provided a `hostname` option which is not a primitive string, the function returns a type error', function test( t ) {
+	var values;
+	var err;
+	var i;
+
+	values = [
+		5,
+		NaN,
+		null,
+		undefined,
+		true,
+		[],
+		{},
+		function(){}
+	];
+
+	for ( i = 0; i < values.length; i++ ) {
+		err = validate( {}, {
+			'hostname': values[i]
+		});
+		t.ok( err instanceof TypeError, 'returns type error when provided ' + values[i] );
+	}
+	t.end();
+});
+
+tape( 'if provided a `port` option which is not a nonnegative integer, the function returns a type error', function test( t ) {
+	var values;
+	var err;
+	var i;
+
+	values = [
+		'5',
+		Math.PI,
+		-1,
+		NaN,
+		null,
+		undefined,
+		true,
+		[],
+		{},
+		function(){}
+	];
+
+	for ( i = 0; i < values.length; i++ ) {
+		err = validate( {}, {
+			'port': values[i]
+		});
+		t.ok( err instanceof TypeError, 'returns type error when provided ' + values[i] );
+	}
+	t.end();
+});
+
+tape( 'if provided a `path` option which is not a primitive string, the function returns a type error', function test( t ) {
+	var values;
+	var err;
+	var i;
+
+	values = [
+		5,
+		NaN,
+		null,
+		undefined,
+		true,
+		[],
+		{},
+		function(){}
+	];
+
+	for ( i = 0; i < values.length; i++ ) {
+		err = validate( {}, {
+			'path': values[i]
+		});
+		t.ok( err instanceof TypeError, 'returns type error when provided ' + values[i] );
+	}
+	t.end();
+});
+
+tape( 'if provided a `page` option which is not a positive integer, the function returns a type error', function test( t ) {
+	var values;
+	var err;
+	var i;
+
+	values = [
+		'5',
+		-1,
+		Math.PI,
+		NaN,
+		null,
+		undefined,
+		true,
+		[],
+		{},
+		function(){}
+	];
+
+	for ( i = 0; i < values.length; i++ ) {
+		err = validate( {}, {
+			'page': values[i]
+		});
+		t.ok( err instanceof TypeError, 'returns type error when provided ' + values[i] );
+	}
+	t.end();
+});
+
+tape( 'if provided a `last_page` option which is not a positive integer, the function returns a type error', function test( t ) {
+	var values;
+	var err;
+	var i;
+
+	values = [
+		'5',
+		-1,
+		Math.PI,
+		NaN,
+		null,
+		undefined,
+		true,
+		[],
+		{},
+		function(){}
+	];
+
+	for ( i = 0; i < values.length; i++ ) {
+		err = validate( {}, {
+			'last_page': values[i]
+		});
+		t.ok( err instanceof TypeError, 'returns type error when provided ' + values[i] );
+	}
+	t.end();
+});
+
+tape( 'if provided a `per_page` option which is not a positive integer, the function returns a type error', function test( t ) {
+	var values;
+	var err;
+	var i;
+
+	values = [
+		'5',
+		-1,
+		Math.PI,
+		NaN,
+		null,
+		undefined,
+		true,
+		[],
+		{},
+		function(){}
+	];
+
+	for ( i = 0; i < values.length; i++ ) {
+		err = validate( {}, {
+			'per_page': values[i]
+		});
+		t.ok( err instanceof TypeError, 'returns type error when provided ' + values[i] );
+	}
+	t.end();
+});
+
+tape( 'if provided a `token` option which is not a primitive string, the function returns a type error', function test( t ) {
+	var values;
+	var err;
+	var i;
+
+	values = [
+		5,
+		NaN,
+		null,
+		undefined,
+		true,
+		[],
+		{},
+		function(){}
+	];
+
+	for ( i = 0; i < values.length; i++ ) {
+		err = validate( {}, {
+			'token': values[i]
+		});
+		t.ok( err instanceof TypeError, 'returns type error when provided ' + values[i] );
+	}
+	t.end();
+});
+
+tape( 'if provided an `accept` option which is not a primitive string, the function returns a type error', function test( t ) {
+	var values;
+	var err;
+	var i;
+
+	values = [
+		5,
+		NaN,
+		null,
+		undefined,
+		true,
+		[],
+		{},
+		function(){}
+	];
+
+	for ( i = 0; i < values.length; i++ ) {
+		err = validate( {}, {
+			'accept': values[i]
+		});
+		t.ok( err instanceof TypeError, 'returns type error when provided ' + values[i] );
+	}
+	t.end();
+});
+
+tape( 'if provided a `useragent` option which is not a primitive string, the function returns a type error', function test( t ) {
+	var values;
+	var err;
+	var i;
+
+	values = [
+		5,
+		NaN,
+		null,
+		undefined,
+		true,
+		[],
+		{},
+		function(){}
+	];
+
+	for ( i = 0; i < values.length; i++ ) {
+		err = validate( {}, {
+			'useragent': values[i]
+		});
+		t.ok( err instanceof TypeError, 'returns type error when provided ' + values[i] );
+	}
+	t.end();
+});
+
 tape( 'the function returns `null` if all options are valid', function test( t ) {
 	var err;
 
 	err = validate( {}, {
-		// TODO
+		'protocol': 'http',
+		'hostname': 'beep.com',
+		'port': 8080,
+		'path': '/api/v3',
+		'page': 2,
+		'last_page': 5,
+		'per_page': 30,
+		'token': 'abcdefg',
+		'accept': 'beepboop',
+		'useragent': 'beeper-booper'
 	});
-	t.equal( err, null );
+	t.equal( err, null, 'returns null' );
 
 	t.end();
 });
@@ -51,9 +341,12 @@ tape( 'the function will ignore unrecognized options', function test( t ) {
 	var err;
 
 	err = validate( {}, {
-		// TODO
+		'beep': 'boop',
+		'a': 5,
+		'b': null,
+		'c': 'woot'
 	});
-	t.equal( err, null );
+	t.equal( err, null, 'returns null' );
 
 	t.end();
 });
